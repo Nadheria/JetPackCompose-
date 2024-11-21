8d874bcc568828995fa3449c98e5440d29d034e5
Govind <somgov0@gmail.com>
2024-11-21 14:29:16 +0530

    Create A List View using Row Column

--- app/src/main/java/com/example/composeproject/MainActivity.kt
+++ app/src/main/java/com/example/composeproject/MainActivity.kt
@@ -7,13 +7,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -28,33 +32,67 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-
 @Composable
 fun PrintText() {
-    Text(text = "Hello Govind",
+    Text(
+        text = "Hello Exe",
         fontSize = 30.sp,
         color = Color.Green,
         fontStyle = FontStyle.Italic,
 
 
-    )
+        )
 }
 
 @Composable
 fun RenderImage() {
 
-    Image(painter = painterResource(id = R.drawable.home_icon), contentDescription = "")
+    Image(painter = painterResource(id = R.drawable.user_profile_ic), contentDescription = "")
 }
 
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
-fun PreiviewFuncation(){
-    Column {
-        PrintText()
-        RenderImage()
-    }
+@Preview(showBackground = true, showSystemUi = true, widthDp =300, heightDp = 500)
+fun PreiviewFuncation() {
 
+    Column {
+        ListViewItems(R.drawable.user_profile_ic,"Jhone Doe","Dev")
+        ListViewItems(R.drawable.user_profile_ic,"James","Designer")
+        ListViewItems(R.drawable.user_profile_ic,"Annie","Lead")
+    }
 }
 
+
+@Composable
+fun ListViewItems(listImageView:Int,name:String,occupation:String ){
+    Row(Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = listImageView),
+            contentDescription = "",
+            Modifier.size(48.dp),
+
+
+
+            )
+
+        Column(Modifier.padding(start = 8.dp)) {
+            Text(
+                text = name,
+                fontSize = 20.sp,
+                color = Color.Black,
+                fontStyle = FontStyle.Italic,
+
+                )
+
+            Text(
+                text = occupation,
+                fontSize = 20.sp,
+                color = Color.Gray,
+                fontStyle = FontStyle.Italic,
+
+                )
+        }
+
+
+    }
+}
